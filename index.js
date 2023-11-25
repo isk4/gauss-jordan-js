be4d39398792a15e59b80e486527f3d16e7665e6
Víctor Iskándar <vgutierrezdiban@protonmail.com>
2023-11-25 16:49:53 -0300

    Agregar funciones de fracciones

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// Matrices de prueba
+
 let mat1 = [
   [1, 0, 0, 0],
   [0, 0, 0, 0],
@@ -17,8 +19,72 @@ let mat3 = [
   [7, 6, 13, 1, 19]
 ];
 
+// Funciones fracciones
+
+const enteroAFraccion = (entero) => {
+  return {num: entero, den: 1};
+}
+
+const matrizEnterosAFraccion = (matriz) => {
+  return matriz = matriz.map((fila) => fila.map((elemento) => enteroAFraccion(elemento)));
+}
+
+const formatoFraccion = (frac) => frac.num + (frac.den === 1 ? "" : "/" + frac.den);
+
+const mcd = (a, b) => {
+  let x = Math.abs(a);
+  let y = Math.abs(b);
+
+  if (x < y) {
+    let aux = x;
+    x = y;
+    y = aux;
+  }
+
+  while (true) {
+    if (y == 0) return x;
+    x %= y;
+    if (x == 0) return y;
+    y %= x;
+  }
+}
+
+const mcm = (a, b) => a * b / mcd(a, b); 
+
+const reducir = (frac) => {
+  let divisor = mcd(frac.num, frac.den);
+  return {num: frac.num / divisor, den: frac.den / divisor};
+}
+
+const suma = (frac1, frac2) => {
+  denominador = mcm(frac1.den, frac2.den);
+  let factor1 = denominador / frac1.den;
+  let factor2 = denominador / frac2.den;
+
+  let resultado = {num: (frac1.num * factor1) + (frac2.num * factor2), den: denominador};
+
+  return reducir(resultado);
+}
+
+const multiplicacion = (frac1, frac2) => {
+  let resultado = {num: frac1.num * frac2.num, den: frac1.den * frac2.den};
+  return reducir(resultado);
+}
+
+// Funciones matrices
+
 const mostrar = (matriz) => {
   matriz.forEach((fila) => console.log(fila));
+  console.log("");
+}
+
+const mostrarConFracciones = (matriz) => {
+  matriz.forEach((fila) => {
+    let linea = "[ ";
+    fila.forEach((elemento) => linea += (formatoFraccion(elemento) + " "));
+    linea += "]";
+    console.log(linea);
+  });
   console.log("");
 }
 
@@ -117,5 +183,7 @@ const gaussJordan = (matriz) => {
 
 console.log("Matriz original\n");
 mostrar(mat1);
-console.log("\nEscalonando...\n");
-gaussJordan(mat1);
+//console.log("\nEscalonando...\n");
+//gaussJordan(mat1);
+
+mostrarConFracciones(matrizEnterosAFraccion(mat1));
